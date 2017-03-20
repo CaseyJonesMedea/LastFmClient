b8c6ede03a17752f914b5b96769f281103daadb3
Viachaslau <kuhto@mrsoft.by>
2017-03-20 14:28:14 +0300

    Поменял ориентацию youtube activity. Перенес лайк в тулбар

--- app/src/main/java/by/viachaslau/kukhto/lastfmclient/Activities/YouTubeActivity/YouTubeActivity.java
+++ app/src/main/java/by/viachaslau/kukhto/lastfmclient/Activities/YouTubeActivity/YouTubeActivity.java
@@ -5,6 +5,9 @@ import android.content.pm.ActivityInfo;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.design.widget.FloatingActionButton;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.Toast;
@@ -12,6 +15,7 @@ import android.widget.Toast;
 import com.google.android.youtube.player.YouTubeBaseActivity;
 import com.google.android.youtube.player.YouTubeInitializationResult;
 import com.google.android.youtube.player.YouTubePlayer;
+import com.google.android.youtube.player.YouTubePlayerSupportFragment;
 import com.google.android.youtube.player.YouTubePlayerView;
 
 import by.viachaslau.kukhto.lastfmclient.Others.YouTube;
@@ -21,19 +25,22 @@ import by.viachaslau.kukhto.lastfmclient.R;
  * Created by kuhto on 15.03.2017.
  */
 
-public class YouTubeActivity extends YouTubeBaseActivity implements YouTubePlayer.OnInitializedListener, YouTubeActivityIView, View.OnClickListener {
+public class YouTubeActivity extends AppCompatActivity implements YouTubePlayer.OnInitializedListener,
+        YouTubePlayer.PlaybackEventListener, YouTubeActivityIView, View.OnClickListener, YouTubePlayer.PlayerStateChangeListener {
+
+    public static final String TAG = YouTubeActivity.class.getSimpleName();
 
     public static final String YOUTUBE_CODE = "codeForYouTube";
     public static final String YOUTUBE_TRACK = "youtubeTrack";
 
     private static final int RECOVERY_REQUEST = 1;
-    private YouTubePlayerView player;
+    private YouTubePlayerSupportFragment youTubePlayerSupportFragment;
 
     private YouTubeActivityPresenter presenter;
 
     private ImageView btnShare;
 
-    private FloatingActionButton btnLove;
+    private ImageView btnLove;
 
     private String youTubeCode = "";
 
@@ -41,26 +48,30 @@ public class YouTubeActivity extends YouTubeBaseActivity implements YouTubePlaye
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_youtube);
-        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
         Intent intent = getIntent();
         youTubeCode = intent.getStringExtra(YOUTUBE_CODE);
         initViews();
         presenter = new YouTubeActivityPresenter(this, this, intent);
-        player.initialize(YouTube.key, this);
-
+        youTubePlayerSupportFragment = YouTubePlayerSupportFragment.newInstance();
+        youTubePlayerSupportFragment.initialize(YouTube.key, this);
+        FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+        fragmentTransaction.replace(R.id.youtube_container, youTubePlayerSupportFragment, youTubePlayerSupportFragment.getTag());
+        fragmentTransaction.commitAllowingStateLoss();
     }
 
     private void initViews() {
-        player = (YouTubePlayerView) findViewById(R.id.youtube_view);
         btnShare = (ImageView) findViewById(R.id.btn_share);
         btnShare.setOnClickListener(this);
-        btnLove = (FloatingActionButton)findViewById(R.id.float_btn);
+        btnLove = (ImageView) findViewById(R.id.float_btn);
         btnLove.setOnClickListener(this);
     }
 
     @Override
     public void onInitializationSuccess(YouTubePlayer.Provider provider, YouTubePlayer youTubePlayer, boolean wasRestored) {
         if (!wasRestored) {
+            youTubePlayer.setPlaybackEventListener(this);
+            youTubePlayer.setPlayerStateChangeListener(this);
             youTubePlayer.cueVideo(youTubeCode);
         }
     }
@@ -79,13 +90,17 @@ public class YouTubeActivity extends YouTubeBaseActivity implements YouTubePlaye
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         if (requestCode == RECOVERY_REQUEST) {
             // Retry initialization if user performed a recovery action
-            getYouTubePlayerProvider().initialize(YouTube.key, this);
+            getYouTubePlayerSupportFragment().initialize(YouTube.key, this);
         }
     }
 
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+    }
 
-    protected YouTubePlayer.Provider getYouTubePlayerProvider() {
-        return player;
+    protected YouTubePlayerSupportFragment getYouTubePlayerSupportFragment() {
+        return youTubePlayerSupportFragment;
     }
 
 
@@ -109,5 +124,60 @@ public class YouTubeActivity extends YouTubeBaseActivity implements YouTubePlaye
                 presenter.onBtnLoveClick();
                 break;
         }
+    }
+
+    @Override
+    public void onPlaying() {
+        Log.d(TAG, "onPlaying");
+    }
+
+    @Override
+    public void onPaused() {
+        Log.d(TAG, "onPaused");
+    }
+
+    @Override
+    public void onStopped() {
+        Log.d(TAG, "onStopped");
+    }
+
+    @Override
+    public void onBuffering(boolean b) {
+        Log.d(TAG, "onBuffering");
+    }
+
+    @Override
+    public void onSeekTo(int i) {
+        Log.d(TAG, "onSeekTo");
+    }
+
+    @Override
+    public void onLoading() {
+        Log.d(TAG, "onLoading");
+    }
+
+    @Override
+    public void onLoaded(String s) {
+        Log.d(TAG, "onLoaded" + s);
+    }
+
+    @Override
+    public void onAdStarted() {
+        Log.d(TAG, "onAdStarted");
+    }
+
+    @Override
+    public void onVideoStarted() {
+        Log.d(TAG, "onVideoStarted");
+    }
+
+    @Override
+    public void onVideoEnded() {
+        Log.d(TAG, "onVideoEnded");
+    }
+
+    @Override
+    public void onError(YouTubePlayer.ErrorReason errorReason) {
+        Log.d(TAG, "" + errorReason);
     }
 }
